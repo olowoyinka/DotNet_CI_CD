c430d2173770fefca22da5c4cd74a37ee554fded
Fela Fela <olowofelayinka@gmail.com>
2022-12-30 07:28:14 +0100

    Update WeatherForecastController.cs

--- dotNetCI/Controllers/WeatherForecastController.cs
+++ dotNetCI/Controllers/WeatherForecastController.cs
@@ -42,4 +42,16 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpGet("/Yinka")]
+    public IEnumerable<WeatherForecast> GetYinka()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
