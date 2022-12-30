1c3470f9fe95b40a45fecf34a1bd33b052b134f9
Fela Fela <olowofelayinka@gmail.com>
2022-12-30 07:38:51 +0100

    Updated

--- dotNetCI/Controllers/WeatherForecastController.cs
+++ dotNetCI/Controllers/WeatherForecastController.cs
@@ -54,4 +54,17 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+
+    [HttpGet("/Lanre")]
+    public IEnumerable<WeatherForecast> GetLanre()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
